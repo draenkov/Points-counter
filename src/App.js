cb1cc81fc760d9873598a6d36789d0fb9781432d
Vyacheslav Draenkov <vyacheslav.draenkov@idfinance.com>
2023-12-31 00:40:43 +0300

    feat: implement points counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Main from "./pages/Main";
+import Admin from "./pages/Admin";
 
-function App() {
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element:  <Main />,
+    },
+    {
+        path: "/admin",
+        element:  <Admin />,
+    },
+]);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <RouterProvider router={router} />
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBPhShCb-Bay6mDR2jYvT8b8Ed1XdFYUN4",
+    authDomain: "pointscounter-ab013.firebaseapp.com",
+    projectId: "pointscounter-ab013",
+    storageBucket: "pointscounter-ab013.appspot.com",
+    messagingSenderId: "965474937044",
+    appId: "1:965474937044:web:49101e6fd27873710eb341",
+    measurementId: "G-11JRD2E2GE",
+    databaseURL: "https://pointscounter-ab013-default-rtdb.europe-west1.firebasedatabase.app",
+};
+
+initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +25,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
